6b49d262c9414bd87709ba752004aed02d4ca024
WHITESKY0109\조건우 <whitesky0109@naver.com>
2021-03-20 00:48:59 +0900

    [master][config] fixed lint error

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,48 +1,44 @@
-'use strict';
+const {
+  url, title, tagline, author, labels,
+} = require('./config');
 
-const siteConfig = require('./config');
+const siteMetadata = {
+  url,
+  title,
+  tagline,
+  description: "A blog template for web developers that's ready to go out of the box. Feel free to modify it to your liking.",
+  author: author.name,
+  contacts: author.contacts,
+  labels,
+};
 
 module.exports = {
-  siteMetadata: {
-    url: siteConfig.url,
-    title: siteConfig.title,
-    tagline: siteConfig.tagline,
-    description: `A blog template for web developers that's ready to go out of the box. Feel free to modify it to your liking.`,
-    author: siteConfig.author.name,
-    contacts: {
-      linkedin: siteConfig.author.contacts.linkedin,
-      github: siteConfig.author.contacts.github,
-      stackoverflow: siteConfig.author.contacts.stackoverflow,
-      freecodecamp: siteConfig.author.contacts.freecodecamp,
-      twitter: siteConfig.author.contacts.twitter,
-    },
-    labels: siteConfig.labels,
-  },
+  siteMetadata,
   plugins: [
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/posts`,
-        name: `blog`,
+        name: 'blog',
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-prismjs`,
+            resolve: 'gatsby-remark-prismjs',
             options: {
               classPrefix: 'language-',
               inlineCodeMarker: null,
@@ -51,9 +47,9 @@ module.exports = {
               noInlineHighlight: false,
             },
           },
-          `gatsby-remark-responsive-iframe`,
+          'gatsby-remark-responsive-iframe',
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-images',
             options: {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
@@ -65,27 +61,28 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-fonts`,
+      resolve: 'gatsby-plugin-google-fonts',
       options: {
         fonts: [
-          `Raleway`,
-          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
+          'Raleway',
+          // eslint-disable-next-line no-useless-escape
+          'source sans pro\:300,400,400i,700', // you can also specify font weights and styles
         ],
       },
     },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-netlify-cms`,
+    'gatsby-plugin-netlify-cms',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
